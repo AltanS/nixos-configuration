08faf842ee0aed13a344854c7288f54c8edbc09d
Altan Sarisin <altansarisin@gmail.com>
2025-04-01 22:08:21 +0200

    Update NixOS configuration: remove home-manager module from default.nix and enable experimental features and unfree packages in nix.nix.

--- modules/default.nix
+++ modules/default.nix
@@ -3,7 +3,6 @@
     ./audio.nix
     ./bluetooth.nix
     ./boot.nix
-    ./home-manager.nix
     ./hyprland.nix
     ./nix.nix
     ./timezone.nix
--- modules/nix.nix
+++ modules/nix.nix
@@ -1,3 +1,10 @@
 {
+  # Enable flakes and nix-command
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  # Allow unfree packages system-wide
+  nixpkgs.config = {
+    # Basic setting to allow unfree packages
+    allowUnfree = true;
+  };
 }
