af8f79ef5a397e2e7ba935c578f36120e9e1dd87
Altan Sarisin <altansarisin@gmail.com>
2025-04-01 22:29:08 +0200

    Refactor NixOS VM configuration: remove VM-specific Wayland settings and X11 support to streamline environment setup.

--- hosts/nixos-vm-conqueror/configuration.nix
+++ hosts/nixos-vm-conqueror/configuration.nix
@@ -16,27 +16,6 @@
   boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = false;
 
-  # VM-specific Wayland settings
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    extraPackages = with pkgs; [ mesa.drivers ];
-  };
-  
-  # Essential environment variables for Wayland in a VM
-  environment.sessionVariables = {
-    # Essential for Wayland compositors in VMs
-    WLR_NO_HARDWARE_CURSORS = "1";
-    WLR_RENDERER_ALLOW_SOFTWARE = "1";
-    # For better scaling
-    GDK_SCALE = "1";
-    # Enable fractional scaling
-    GDK_DPI_SCALE = "1";
-  };
-
-  # Minimal X11 support for GUI applications that might need it
-  services.xserver.enable = true;
-  
   environment.systemPackages = [ pkgs.home-manager ];
 
   networking.hostName = hostname;
