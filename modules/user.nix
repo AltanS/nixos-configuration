819be3f85520bec1f0d68de4f5fee1923ebd1efa
Altan Sarisin <altansarisin@gmail.com>
2025-04-01 22:53:56 +0200

    Remove Zsh configuration from user module: disable Zsh as the default user shell and remove related settings for a cleaner user setup.

--- modules/user.nix
+++ modules/user.nix
@@ -1,8 +1,5 @@
 { pkgs, user, ... }: {
-  programs.zsh.enable = true;
-
   users = {
-    defaultUserShell = pkgs.zsh;
     users.${user} = {
       isNormalUser = true;
       extraGroups = [ "wheel" "networkmanager" ];
