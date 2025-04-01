975a0fbbdde10ad59cc95a0dc233f7ff1f45f661
Altan Sarisin <altansarisin@gmail.com>
2025-04-01 21:53:23 +0200

    Implement direct boot configuration for NixOS VM using GRUB and disable systemd-boot. Update boot.nix to remove default boot settings.

--- hosts/nixos-vm-conqueror/configuration.nix
+++ hosts/nixos-vm-conqueror/configuration.nix
@@ -7,6 +7,15 @@
     ../../modules
   ];
 
+  # Direct boot configuration for VM
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
+  
+  # Explicitly disable systemd-boot
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = false;
+
   environment.systemPackages = [ pkgs.home-manager ];
 
   networking.hostName = hostname;
