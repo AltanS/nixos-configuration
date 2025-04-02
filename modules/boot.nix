8f76986fbc31035164a928dd7f25cd441ae658e3
Altan Sarisin <altansarisin@gmail.com>
2025-04-02 21:36:33 +0200

    asd

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,6 +1,4 @@
-{ config, lib, pkgs, ... }:
-
 {
-  # This module doesn't set any boot configuration by default
-  # Boot configuration will be specified directly in each host's configuration
-} 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+}
